0c1c9e2350b9f28b65df179c071d649250072d13
g33f <g33e33f@gmail.com>
2020-06-10 13:18:54 +0300

    s3

--- srcs/drow.c
+++ srcs/drow.c
@@ -19,7 +19,7 @@ void	draw(t_data *p, size_t i, size_t column_h)
 
 	r = 0;
 	space = p->mlx.win_y_size - column_h;
-	while(r < column_h)
+	while(r < (int)column_h)
 	{
 		p->mlx.img_data[i + (r + space) * p->mlx.win_x_size] = 0xFFFFFF;
 		p->mlx.img_data[i + 1 + (r + space) * p->mlx.win_x_size] = 0xFFFFFF;
@@ -36,11 +36,11 @@ void	cast(t_data *p, float angel, size_t i)
 	t = 0;
 	while(t < 20.0)
 	{
-		cx = player_x + t * cos(angel);
-		cy = player_y + t * sin(angel);
+		cx = p->ply.x + t * cos(angel);
+		cy = p->ply.y + t * sin(angel);
 		if (p->map.map[int(cx)][int(cy)] != '0')
 		{
-			column_h = p->mlx.win_y_size/t
+			column_h = p->mlx.win_y_size/t;
 			draw(p, i, column_h);
 		}
 		t += 0.05;
@@ -53,9 +53,9 @@ void	render(t_data *p)
 	size_t	i;
 
 	i = 0;
-	for (i++ < win_w/2)
+	for (i++ < p->mlx.win_x_size/2)
 	{
-		angle = p->ply.a - p->ply.fov / 2 + p->ply.fov * i / float(win_w/2);
+		angle = p->ply.a - p->ply.fov / 2 + p->ply.fov * i / float(p->mlx.win_x_size /2);
 		cast(p, angle, i);
 	}
 }
